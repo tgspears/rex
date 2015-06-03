15c4fef3b19cc6c856814147304f56f35817e334
laurenngo <lauren.nicole.go@gmail.com>
2015-06-03 13:52:04 -0700

    yelp search working and results populating. Also added hidden form for data transfer

--- api/controllers/RexController.js
+++ api/controllers/RexController.js
@@ -5,18 +5,30 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+// var yelp = require("yelp").createClient({
+//   consumer_key: 'DGhvxpFiEk-RNYqp7BcjDw',
+//   consumer_secret: 'ihXNLNUwBj58u2uTDF0POmvmD4Q',
+//   token: '7CzABFWKgI3FecwjmyZG-ThVGB-XfVdJ',
+//   token_secret: 'JS84EZg8qIh5sVOimgnyIzuKSXM'
+// });
+
 var yelp = require("yelp").createClient({
-  consumer_key: 'DGhvxpFiEk-RNYqp7BcjDw',
-  consumer_secret: 'ihXNLNUwBj58u2uTDF0POmvmD4Q',
-  token: '7CzABFWKgI3FecwjmyZG-ThVGB-XfVdJ',
-  token_secret: 'JS84EZg8qIh5sVOimgnyIzuKSXM'
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_CONSUMER_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET
 });
+
 
 module.exports = {
   yelpSearch: function(req,res){
-	  yelp.search({term:req.body.term, location:req.body.location}, function(error,data){
+  	console.log(req.params);
+	  yelp.search({term:req.query.term, location:req.query.location}, function(error,data){
 	  	console.log(data)
 	  	res.send({data: data, error: error})
 	  });
+	// res.send(req.params)
+
 	}
+
 };
