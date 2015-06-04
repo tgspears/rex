7daf41419ba382ab6b907bc1618ca5b325b492a1
laurenngo <lauren.nicole.go@gmail.com>
2015-06-04 00:50:20 -0700

    fixed signup modals and added list modal. started some styling.

--- assets/js/app/controllers_F/authModalCtrl.js
+++ assets/js/app/controllers_F/authModalCtrl.js
@@ -1,4 +1,4 @@
-RexApp.controller('authModalCtrl', ['$scope', 'UserService', '$modal', function($scope,UserService,$modal){
+RexApp.controller('authModalCtrl', ['$scope', 'UserService', '$modal', '$http', function($scope,UserService,$modal,$http){
   console.log("MY AUTH CTRL LOADED");
 
   $scope.login = function(){
@@ -11,6 +11,21 @@ RexApp.controller('authModalCtrl', ['$scope', 'UserService', '$modal', function(
   			alert('Invalid authentication credentials. Please try again.');
   		}
   	})
+  }
+
+  $scope.signup = function(){
+    console.log("signup!")
+
+    var newUser = {
+      username:$scope.newUser.username,
+      email:$scope.newUser.email,
+      password:$scope.newUser.password
+    }
+
+    $http.post('/api/user', newUser)
+    .success(function(data){
+      console.log('WORKING'+data)
+    })
   }
 
 }])
